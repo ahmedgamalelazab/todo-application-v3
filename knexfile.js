36ff17e1e3563c2b1b35cca76c0098276830cb2a
= <=>
2021-08-09 22:07:15 +0300

    final fix

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString:process.env.DATABASE_URL,
-      ssl:true,
+      ssl:{ rejectUnauthorized: false },
     },
     
     migrations: {
